5c7382acf935238b34ff4e8969ee29ee9227d3ac
Kyle-Gage <gageka@mail.uc.edu>
2022-02-06 16:55:30 -0500

    Variable declarations for Lab04

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+/*
+ the number of kids in your family: 3
+ the answer to is it raining? : no
+ the price of a gallon of gas :3.50
+ your favorite number : 15
+ your shoe size: 10
+ your birth month: October
+your full name: Kyle Andrew Gage
+ */
+    public static void main(String[] args)
+    {
+	int kidsInFamily = 3;
+    boolean isRaining = false;
+    double pricePerGallon = 3.50;
+    int favNum = 15;
+    int shoeSize = 10;
+    String birthMonth =  "October";
+    String fullName = "Kyle Andrew Gage";
+
+    }
+}
